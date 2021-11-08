1bb4614fd42e68d38db6c9c4668e7f071567e11b
HanJungwoo1102 <ajtwlswjddnv1102@gmail.com>
2021-11-08 11:46:36 +0900

    feat:model, config 추가

--- src/app.py
+++ src/app.py
@@ -1,15 +1,20 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from dotory_fairy_tale_generator import get_model, get_tokenizer, generate_sentences
+import torch
 
 class App:
     def __init__(self):
         app = Flask(__name__)
         CORS(app)
 
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        model = get_model('model.pt', 'config.json', device)
+        tokenizer = get_tokenizer()
+
         @app.route('/', methods=('GET', ))
         def route_get_home():
-            print(get_model)
+            sentences = generate_sentences(model, tokenizer, '옛날에 한 소녀가 살고 있었어요.', device)
             return 'dotory fairy tale generator server'
 
         @app.route('/v1/models/model/infer', methods=('POST', ))

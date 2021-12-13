b45c011a4732404c371477b0292a416afe2a016b
HanJungwoo1102 <ajtwlswjddnv1102@gmail.com>
2021-12-13 15:17:11 +0900

    feat:test

--- src/app.py
+++ src/app.py
@@ -1,6 +1,14 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, send_file
 from flask_cors import CORS
 from dotory_fairy_tale_generator import FairyTaleGenerator
+from dotory_fairy_tale_image_style_transferer import StyleTransferer
+from PIL import Image
+from datetime import datetime
+import os
+
+def get_extenstion_of_file_name(file_name):
+    return file_name.split('.')[-1]
+
 
 class App:
     def __init__(self):
@@ -10,6 +18,7 @@ class App:
         app.config['JSON_AS_ASCII'] = False
 
         fairy_tale_generator = FairyTaleGenerator('trained/checkpoint.tar', 'trained/', 'trained/config.json')
+        style_transferer = StyleTransferer(Image.open('resources/image/abstract-image-13.jpeg'))
 
         @app.route('/', methods=('GET', ))
         def route_get_home():
@@ -31,15 +40,22 @@ class App:
         
         @app.route('/v1/test2', methods=('GET', ))
         def route_test2():
-            input_sentence = request.args.get('sentence')
-            output_sentences = fairy_tale_generator.generate(input_sentence)
-            
-            return jsonify({
-                'data': {
-                    'input_sentence': input_sentence,
-                    'output_sentences': output_sentences,
-                },
-            })
+            image_dir_path = "resources/temp_image/"
+            datetime_string = datetime.today().strftime("%Y%m%d%H%M%S")
+            file = request.files['image']
+            file_path = image_dir_path + datetime_string + "-" + file.filename
+            os.makedirs(image_dir_path, exists_ok=True)
+            file.save(file_path)
+
+            style_transfered_image = style_transferer.transfer(Image.open(file_path))
+        
+            result_image_dir_path = "resources/result_image"
+            result_file_path = result_image_dir_path + datetime_string + file.filename
+            os.makedirs(result_image_dir_path, exists_ok=True)
+            style_transfered_image.save(result_file_path)
+
+            return send_file(result_file_path, mimetype='image/' + get_extenstion_of_file_name(file.filename))
+        
         
         @app.route('/v1/test3', methods=('POST', ))
         def route_test3():

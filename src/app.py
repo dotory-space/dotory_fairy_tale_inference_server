589ff8a58ffac29b6b9c7af89f1f91b5c6000f0c
HanJungwoo1102 <ajtwlswjddnv1102@gmail.com>
2021-12-23 09:46:02 +0900

    feat:dir 변경

--- src/app.py
+++ src/app.py
@@ -41,7 +41,7 @@ class App:
         @app.route('/v1/fairy-tale-images', methods=('POST', ))
         def route_fairy_tale_images():
             working_dir_path = os.getcwd()
-            image_dir_path = working_dir_path + "resources/temp_image/"
+            image_dir_path = working_dir_path + "/resources/temp_image/"
             datetime_string = datetime.today().strftime("%Y%m%d%H%M%S")
             file = request.files['image']
             file_path = image_dir_path + datetime_string + "-" + file.filename
@@ -50,7 +50,7 @@ class App:
 
             style_transfered_image = style_transferer.transfer(Image.open('resources/image/abstract-image-' + str(random.randrange(1, 29)) + '.jpeg'), Image.open(file_path))
         
-            result_image_dir_path = working_dir_path + "resources/result_image/"
+            result_image_dir_path = working_dir_path + "/resources/result_image/"
             result_file_path = result_image_dir_path + datetime_string + "-" + file.filename
             os.makedirs(result_image_dir_path, exist_ok=True)
             style_transfered_image.save(result_file_path)
